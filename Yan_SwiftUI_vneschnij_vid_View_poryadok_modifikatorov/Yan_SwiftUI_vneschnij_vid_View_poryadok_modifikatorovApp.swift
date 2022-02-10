6f273f51047f73f97b6d3f570ced4f7c8051f9b6
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 04:55:14 +0500

    Initial Commit

---
+++ Yan_SwiftUI_vneschnij_vid_View_poryadok_modifikatorov/Yan_SwiftUI_vneschnij_vid_View_poryadok_modifikatorovApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_vneschnij_vid_View_poryadok_modifikatorovApp.swift
+//  Yan_SwiftUI_vneschnij_vid_View_poryadok_modifikatorov
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_vneschnij_vid_View_poryadok_modifikatorovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
